e5cedb0cbbc3d9eaa15dc91a5005adcbae7ad259
jxkhoo8@gmail.com <jxkhoo8@gmail.com>
2026-01-16 16:38:16 +0800

    Finish Level-0

---
+++ src/main/java/Hal.java
@@ -0,0 +1,25 @@
+public class Hal {
+    public static void main(String[] args) {
+        String logo = "                           .---. \n"
+         + "       .                   |   | \n"
+         + "     .'|                   |   | \n"
+         + "    <  |                   |   | \n"
+         + "     | |             __    |   | \n"
+         + "     | | .'''-.   .:--.'.  |   | \n"
+         + "     | |/.'''. | / |   | | |   | \n"
+         + "     |  /    | | `' __ | | |   | \n"
+         + "     | |     | |  .'.''| | |   | \n"
+         + "     | |     | | / /   | |_'---' \n"
+         + "     | '.    | '.| |._,| '/      \n"
+         + "     '---'   '---'`--'  `'       \n";
+
+        String divider = "____________________________________________________________\n";
+
+        System.out.println(divider
+                + logo
+                + "Hello! I'm Hal\nWhat can I do for you?\n"
+                + divider
+                + "Bye. Hope to see you again soon!\n"
+                + divider);
+    }
+}
